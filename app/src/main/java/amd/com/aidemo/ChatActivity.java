29af6451601cb86df25e95512cde76d43338a96f
Dragoljub <zivanovic.dragoljub@gmail.com>
2024-06-20 11:31:37 +0200

    Added server communication for chat

--- app/src/main/java/amd/com/aidemo/ChatActivity.java
+++ app/src/main/java/amd/com/aidemo/ChatActivity.java
@@ -1,8 +1,11 @@
 package amd.com.aidemo;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,7 +16,24 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.List;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class ChatActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -21,13 +41,22 @@ public class ChatActivity extends AppCompatActivity {
     private EditText editTextMessage;
     private ImageButton buttonSend;
     private ChatViewModel viewModel;
+    private RequestQueue requestQueue;
+    private String chatId = "";
+    private SharedPreferences sharedPreferences;
+    private String serverIpAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        handleSSLHandshake();
         viewModel = new ViewModelProvider(this).get(ChatViewModel.class);
+        requestQueue = Volley.newRequestQueue(this);
+
+        sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        serverIpAddress = sharedPreferences.getString("ipAddress", "");
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clChatActivity), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -50,9 +79,7 @@ public class ChatActivity extends AppCompatActivity {
                 viewModel.addMessage(userMessage);
                 editTextMessage.setText("");
 
-                // Simulate AI response
-                Message aiMessage = new Message("AI response to: " + messageText, false);
-                viewModel.addMessage(aiMessage);
+                sendMessageToServer(messageText);
             }
         });
 
@@ -63,5 +90,92 @@ public class ChatActivity extends AppCompatActivity {
                 recyclerView.scrollToPosition(messages.size() - 1);
             }
         });
+    }
+
+    private void sendMessageToServer(String messageText) {
+        String url = "https://" + serverIpAddress + "/v1/chat/simple";
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("chat_id", chatId);
+            requestBody.put("message", messageText);
+            requestBody.put("topic_id", "amd");
+            requestBody.put("history", "");
+            requestBody.put("extra", "");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, requestBody,
+                response -> {
+                    try {
+                        chatId = response.getString("chat_id");
+                        if (response.getInt("error") == 0) {
+                            getMessageFromServer(chatId);
+                        } else {
+                            Toast.makeText(ChatActivity.this, "Error: " + response.getInt("error"), Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(ChatActivity.this, "Error in response", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                error -> Toast.makeText(ChatActivity.this, "Can't connect to the server", Toast.LENGTH_SHORT).show());
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void getMessageFromServer(String chatId) {
+        String url = "https://" + serverIpAddress + "/v1/chat/simple?chat_id=" + chatId;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        String aiMessage = response.getString("message");
+                        boolean completed = response.getBoolean("completed");
+                        Message message = new Message(aiMessage, false);
+                        viewModel.addMessage(message);
+
+                        if (!completed) {
+                            // Continue polling until message is completed
+                            getMessageFromServer(chatId);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(ChatActivity.this, "Error in response", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                error -> Toast.makeText(ChatActivity.this, "Can't connect to the server", Toast.LENGTH_SHORT).show());
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    @SuppressLint("TrulyRandom")
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
     }
 }
